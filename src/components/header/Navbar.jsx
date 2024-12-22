e67a6e36bd4067d0caa84111c8a88d3e3cec3f18
Ignas Dauksys <ignasdauksys@protonmail.com>
2024-12-22 10:31:24 -0600

    add: navbar modifications

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,29 +1,27 @@
 const Navbar = () => {
   return (
-    <div className="w-full h-full bg-secondary">
-      <nav>
-        <ul className="flex justify-between items-center p-5 bg-gray-800 text-white">
-          <li>
+    <div className="w-full h-full bg-secondary font">
+      <nav className="relative">
+        <ul className="flex justify-evenly items-center p-5 bg-gray-800 text-white font-firstFont">
+          <li className="px-4">
             <a href="/" className="text-white">
               Home
             </a>
           </li>
-          <li>
+          <li className="px-4">
             <a href="/about" className="text-white">
               About
             </a>
           </li>
-          <li>
-            <a href="/order" className="text-white">
-              Order
-            </a>
+          <li className=" relative flex justify-center">
+            <div className="absolute top-1/2 transform -translate-y-1/2 w-40 h-40 rounded-full mx-auto bg-secondary"></div>
           </li>
-          <li>
+          <li className="px-4">
             <a href="/menu" className="text-white">
               Menu
             </a>
           </li>
-          <li>
+          <li className="px-4">
             <a href="/contact" className="text-white">
               Contact
             </a>
